1da132b265f09655b33d18fd7df509bf2f4ba7f6
Noel Akers <noel@oleana.co.uk>
2026-06-15 17:34:38 +0100

    Seeder changes

--- database/seeders/ShowSectionSeeder.php
+++ database/seeders/ShowSectionSeeder.php
@@ -12,11 +12,12 @@ class ShowSectionSeeder extends Seeder
         ShowSection::create(['sort_order' => 1, 'name' => 'Fruit & Veg', 'description' => 'Fruit & Vegetables']);
         ShowSection::create(['sort_order' => 2, 'name' => 'Giant Veg', 'description' => 'Battle of the Giant Veg!']);
         ShowSection::create(['sort_order' => 3, 'name' => 'Flowers', 'description' => 'Flowers']);
-        ShowSection::create(['sort_order' => 4, 'name' => 'Baking', 'description' => 'Baking']);
-        ShowSection::create(['sort_order' => 5, 'name' => 'Preserves', 'description' => 'Preserves']);
-        ShowSection::create(['sort_order' => 6, 'name' => 'Handicrafts', 'description' => 'Handicrafts']);
-        ShowSection::create(['sort_order' => 7, 'name' => 'Art', 'description' => 'Art']);
-        ShowSection::create(['sort_order' => 8, 'name' => 'Photography', 'description' => 'Photography']);
-        ShowSection::create(['sort_order' => 9, 'name' => 'Juniors', 'description' => 'Classes for juniors: under 11 and 11-15']);
+        ShowSection::create(['sort_order' => 4, 'name' => 'Celebrating Calverley in Bloom', 'description' => 'Fun floral classes']);
+        ShowSection::create(['sort_order' => 5, 'name' => 'Baking', 'description' => 'Baking']);
+        ShowSection::create(['sort_order' => 6, 'name' => 'Preserves', 'description' => 'Preserves']);
+        ShowSection::create(['sort_order' => 7, 'name' => 'Handicrafts', 'description' => 'Handicrafts']);
+        ShowSection::create(['sort_order' => 8, 'name' => 'Art', 'description' => 'Art']);
+        ShowSection::create(['sort_order' => 9, 'name' => 'Photography', 'description' => 'Photography']);
+        ShowSection::create(['sort_order' => 10, 'name' => 'Juniors', 'description' => 'Classes for juniors: under 11 and 11-15']);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,29 +4,28 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
         User::factory()->admin()->create([
-            'name' => 'Noel Akers',
-            'email' => 'noel@oleana.co.uk',
+            'name' => 'Show Admin',
+            'email' => 'admin@villageshow.org',
+            'password' => Hash::make(env('SEEDER_ADMIN_PASSWORD')),
         ]);
 
         User::factory()->judge()->create([
             'name' => 'TBC',
-            'email' => 'tbc@oleana.co.uk',
-        ]);
-
-        User::factory()->judge()->create([
-            'name' => 'Anne Akers',
-            'email' => 'anne.akers@oleana.co.uk',
+            'email' => 'tbc@villageshow.org',
+            'password' => Hash::make(env('SEEDER_JUDGE_PASSWORD')),
         ]);
 
         User::factory()->helper()->create([
             'name' => 'Generic Helper',
-            'email' => 'helper@oleana.co.uk',
+            'email' => 'helper@villageshow.org',
+            'password' => Hash::make(env('SEEDER_HELPER_PASSWORD')),
         ]);
     }
 }
