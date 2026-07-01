36e4e677b7ee23ace4d6cb50ac7b6931782fddca
Noel Akers <noel@oleana.co.uk>
2026-07-01 21:16:12 +0100

    Change to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,19 @@
 
             <main class="flex flex-1 flex-col items-center justify-center gap-10 px-6 py-12">
                 <div class="text-center">
-                    <h1 class="text-5xl font-semibold tracking-tight text-zinc-900">2026 Calverley Show</h1>
+                    <h1 class="text-5xl font-semibold tracking-tight text-zinc-900">Calverley Show</h1>
+                </div>
+
+                <div class="text-center">
+                    <h2 class="text-5xl font-semibold tracking-tight text-zinc-900">Saturday 14 August 2026</h2>
+                </div>
+
+                <div class="text-center">
+                    <h2 class="text-5xl font-semibold tracking-tight text-zinc-900">Calverley Methodist Hall and Church</h2>
+                </div>
+
+                <div class="text-center">
+                    <h2 class="text-5xl font-semibold tracking-tight text-zinc-900">Doors open 1.00 pm</h2>
                 </div>
 
                 <div class="max-w-2xl px-8 py-7 text-center">
