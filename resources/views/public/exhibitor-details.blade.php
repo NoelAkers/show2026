a24857dd461c2a54ff5342744bcda999e8a4f5ac
Noel Akers <noel@oleana.co.uk>
2026-07-05 18:27:27 +0100

    Exhibitor info changes

--- resources/views/public/exhibitor-details.blade.php
+++ resources/views/public/exhibitor-details.blade.php
@@ -7,19 +7,32 @@
             </div>
             <div class="w-full max-w-full sm:max-w-3xl px-4 sm:px-8 py-7 text-justify">
                 <p class="leading-relaxed text-zinc-700">
-                    Calverley Horticultural Society’s 105th show will be held on Saturday 14 August 2026, at the Calverley Methodist Hall and Church. 
-                    Doors open at 1.00 pm by which time judging should be finished and you will be able to access the Fruit and Vegetable sections in the main hall, the Art in the church and 
-                    Baking, Handicrafts and other sections in the school room. Admission is pay as you feel for all visitors.
+                    Calverley Horticultural Society’s 105th show will be held on Saturday 14 August 2026, at the Calverley Methodist Hall and Church, Chapel Street, LS28 5PS. We want to encourage entries from as many
+                     people as possible, you do not have to be a Calverley Horticultural Society (CHS) member to enter. 
                 </p>
                 <p class="mt-4 leading-relaxed text-zinc-700">
-                    There will be live music from the Drighlington Brass Band in the paved area outside the hall, together with food and other stalls. Coffee and cake is available indoors, (as well as limited seating if the weather is unkind), 
-                    along with our always popular Tombola stall. 
+                    Details of the classes can be found in the printed schedule and from the link in the menu. For adults it costs 50p per entry up to 10 entries, after which additional entries are free. 
+                    For juniors (up to age 15), entry is free. We can take payment in cash or by card. Entries can be brought to the Methodist Hall on Friday 13 August 2026 between 6.00 pm and 8.00 pm (preferred), or on Saturday 14 August 2026 
+                    between 9.30 am and 11.30 am. Judging will start promptly at 11.30 am, please allow adequate time for registration and staging if you are entering on Saturday.
+                    Please note that the hall will be closed to exhibitors and visitors during judging. Art entries will be displayed in the church and staged by our stewards.
+                </p>
+                <p class="mt-4 leading-relaxed text-zinc-700">
+                    All entries can be made without prior registration, when you arrive at the show. You will need to provide your name and specify whether you are an adult or junior. Some trophies are restricted to entrants 
+                    who are residents of Calverley, or have an allotment plot in the village and some are only open to novices, i.e. exhibitors who have not previously won a trophy at this or similar shows, 
+                    so we will also need this information at the time of registration. You will also need to provide details of each how many entries you are making in each class and will be provided with a uniquely numbered
+                    printed label for each entry, which you will need to attach to your entry. Paper plates and vases will be provided for your entries where required. Please see the schedule for the (very few) exceptions where the 
+                    exhibitor needs to provide the container. Assistance will be available from our stewards to help you stage your entries, and to answer any questions you may have about the show. If you need to change or add to your entries,
+                    this can be done straightforwardly up to the time when judging commences.
+                </p>
+                    If you would like to save a little time by registering in advance, you can <a href="{{ route('register') }}" class="text-zinc-700 underline underline-offset-2 transition-colors hover:text-zinc-900">register 
+                        here</a> to create an account and submit your entries online up to the Thurday evening before the show. After that time, we can stll make changes when you are physically present for staging in the same way as
+                        for those who have not registered in advance.
                 </p>
                 <p class="mt-4 leading-relaxed text-zinc-700">      
-                    Prizes will be announced and awarded around 4.00 pm after which some items from the Baking, Fruit and Vegetable sections will be available in the schoolroom, for a suitable donation . The show will close at 4.30 pm. For more information about the classes, trophies, timings for visitors and exhibitors and how to enter, click here: <a href="{{ route('public.show-details') }}" class="text-zinc-900 underline underline-offset-2 transition-colors hover:text-zinc-600">Show Details</a>.
-                </p>
-                <p class="mt-4 leading-relaxed text-zinc-700">
-                    If you would like to enter classes in advance of the show, you can <a href="{{ route('register') }}" class="text-zinc-700 underline underline-offset-2 transition-colors hover:text-zinc-900">register here</a> to create an account and submit your entries online.
+                    Trophy winners  will be announced and trophies awarded around 4.00 pm. after which prize money can be collected from the show secretary. If you wish to make any of your entries available for 
+                    sale to visitors, (Vegetables, Fruit, Flowers, Baking, Preserves only), please place a red sticker (available at the show) on that entry's label. The stewards will move these into the schoolroom 
+                    for the post-show auction, all proceeds to Calverley Horticultural Society. Any other items will need to be collected by exhibitors promptly after the show closes at 4.30 pm. The show organisers 
+                    will be very busy with clearing up after the show, so please be considerate and collect your entries promptly. We cannot be held responsible for any items left after 5.30 pm.
                 </p>
             </div>
         </main>
